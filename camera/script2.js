447153bfc94bd0ab5d30ee4e16888a1f471ef313
Sumudeshu <13051565357@163.com>
2024-04-24 23:47:37 +0900

    展示图片

--- camera/script2.js
+++ camera/script2.js
@@ -15,12 +15,21 @@ function makeGetRequest() {
                 <td>${obj.username}</td>
                 <td>${obj.picname}</td>
                 <td>${obj.picpath}</td>
+                <td><img src="http://localhost:8080/file2/getPicture/${obj.id}"></td>
                 <td>${obj.ctime}</td>
                 <td>${obj.mtime}</td>
-                <td>
-                    <input type="button" class='data' value="show more msg">
-                </td>
             </tr>`;
         }
     tr.innerHTML = str;
   }
+
+//   const showPic = document.getElementById('showPic');
+//   showPic.addEventListener('click', showPicture);
+
+//   function showPicture() {
+//     const picPath = showPic.parentNode.previousSibling.innerHTML;
+//     fetch("http://localhost:8080/file2/getPicture" + picPath)
+//     .then(response => response.json())
+//     .then(data => jsonToTable(data.data))
+//     .catch(error => console.error('Error:', error));
+//   }
